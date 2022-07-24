6177b6a93859eee68edc6835f77459b547a6b463
cirow <ciro.perezjr@gmail.com>
2022-07-24 15:19:01 -0300

    more verbose

--- functions/golang/install.sh
+++ functions/golang/install.sh
@@ -6,5 +6,8 @@ GOLANG_VERSION="go1.18.4.linux-amd64.tar.gz"
 
 wget "https://go.dev/dl/${GOLANG_VERSION}"
 rm -rf /usr/local/go && tar -C /usr/local -xzf "${GOLANG_VERSION}"
-[[ -f "${HOME}/.profile" ]] || echo "export PATH=\$PATH:/usr/local/go/bin" > "${HOME}/.profile"
-grep -q "export PATH=\$PATH:/usr/local/go/bin" "${HOME}/.profile" || echo "export PATH=\$PATH:/usr/local/go/bin" >> "${HOME}/.profile"
+[[ -f "/home/${SUDO_USER}/.profile" ]] || (echo "export PATH=\$PATH:/usr/local/go/bin" > "/home/${SUDO_USER}/.profile")
+[[ -f "/home/${SUDO_USER}/.profile" ]] && echo "/home/${SUDO_USER}//.profile found"
+
+echo "Checking for the line at /home/${SUDO_USER}/.profile"
+grep -q "export PATH=\$PATH:/usr/local/go/bin" "/home/${SUDO_USER}//.profile" || (echo "Did not find the line" && echo "export PATH=\$PATH:/usr/local/go/bin" >> "/home/${SUDO_USER}/.profile")
