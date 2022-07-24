305853dfad11b9600beb7c581c211b5b07e99b90
cirow <ciro.perezjr@gmail.com>
2022-07-24 13:11:48 -0300

    fix golang

--- functions/golang/install.sh
+++ functions/golang/install.sh
@@ -4,7 +4,7 @@ NORMAL='\033[0m';
 PARENT_PATH="/home/${SUDO_USER}/.cache/setuper"
 GOLANG_VERSION="go1.18.4.linux-amd64.tar.gz"
 
-wget --output-file="${PARENT_PATH}/go.tar.gz" "https://go.dev/dl/${GOLANG_VERSION}"
-rm -rf /usr/local/go && tar -C /usr/local -xzf "${PARENT_PATH}/go.tar.gz"
+wget "https://go.dev/dl/${GOLANG_VERSION}"
+rm -rf /usr/local/go && tar -C /usr/local -xzf "${GOLANG_VERSION}"
 [[ -f "${HOME}/.profile" ]] || echo "export PATH=\$PATH:/usr/local/go/bin" > "${HOME}/.profile"
 grep -q "export PATH=\$PATH:/usr/local/go/bin" "${HOME}/.profile" || echo "export PATH=\$PATH:/usr/local/go/bin" >> "${HOME}/.profile"
